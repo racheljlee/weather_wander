09148e2c9f818729a8705a2d784407edecd5e7c6
Ben Swolo <rachelkindacodes@gmail.com>
2018-05-04 13:05:50 -0500

    minor update to app.js

--- assets/javascript/apps.js
+++ assets/javascript/apps.js
@@ -1,8 +1,5 @@
 $(document).ready(function() {
-    // // http://api.openweathermap.org/data/2.5/box/city?bbox=-126,25,-66,49,10&appid=4a69e3319a5272bdcee56b147d64
-
-
-
+    // **** BEGIN OPENWEATHER API *** //
     var cityURL = "https://api.openweathermap.org/data/2.5/weather?q=Chicago&appid=";
     var rectangleURL = "https://api.openweathermap.org/data/2.5/box/city?bbox=-126,25,-66,49,10&appid=";
     var APIKEY = "a59b652772e28b82fb2ff69af2f1014c";
@@ -14,4 +11,8 @@ $(document).ready(function() {
             console.log(response);
             $("#testDiv").text(JSON.stringify(response));
         });
+
+
+
+     // *** END OPENWEATHER API *** //   
 })
