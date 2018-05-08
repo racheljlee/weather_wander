314392197225d8ae075a460e415ea4fd39531c5d
Shaikh Rahman <shaikh.s.rahman@gmail.com>
2018-05-08 09:59:39 -0500

    Made changes to Foursquare URL ajax call

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -4,10 +4,10 @@ $(document).ready(function () {
     var APIKEY = "a59b652772e28b82fb2ff69af2f1014c";
     var rectangleURL = "https://api.openweathermap.org/data/2.5/box/city?bbox=-126,25,-66,49,10&units=imperial&appid=";
     cityCounter = 0;
-    
+
     // Hiding the temperature slider when starting the app
     var temperatureSliderDiv = $("#temperature-slider");
-    temperatureSliderDiv.hide(); 
+    temperatureSliderDiv.hide();
 
     // * Temperature Slider slide down animation *
     var thermometerIcon = $("#thermometer-icon");
@@ -22,7 +22,7 @@ $(document).ready(function () {
     slider.on("input", function () {
         output.html(this.value);
     });
-    
+
     $(".temp-btn").on("click", function () {
         console.log("I clicked this");
         $("#accordion").empty();
@@ -51,10 +51,10 @@ $(document).ready(function () {
 
     // ** CREATING ACCORDION TO APPEAR ON DOM **
     function createAccordion(data, cityCounter) {
-       
+
         // console.log("Data: ", data);
         // *** GENERATING ACCORDION + CARD ***
-        var cardDiv = $(`<div class="card">`);
+        var card = $(`<div class="card">`);
         var cardHeader = $(`<div class="card-header">`);
         var cardLink = $(`<a class="card-link" data-toggle="collapse" href="#collapse${cityCounter}" id="cityTab${cityCounter}">`);
         cardLink.attr("data-city", data.name);
@@ -69,11 +69,11 @@ $(document).ready(function () {
         var pDay = $(`<p class="card-text"></p>`);
 
         // *** APPENDING ACCORDION PARTS ***
-        $("#accordion").append(cardDiv);
-        cardDiv.append(cardHeader);
+        $("#accordion").append(card);
+        card.append(cardHeader);
         cardHeader.append(cardLink);
         cardLink.append(data.name);
-        cardDiv.append(collapse);
+        card.append(collapse);
         collapse.append(cardBody);
         cardBody.append(row);
         row.append(col1);
@@ -83,34 +83,59 @@ $(document).ready(function () {
         row.append(col2);
         row.append(col2);
         row.append(col1);
-        
-        return cardDiv;
+
+        return card;
     }
 
     // *** CLICKING ON THE CITY NAME TO SHOW 5 DAY FORECAST
     $(document).on("click", ".card-link", function () {
-        $(this).attr("id");
+        // console.log("what is this: ", $(this).attr("id"));
         console.log($(this).attr("data-city"));
-
+        // card.collapse.show();
+        
         // * Openweather's 5-Day Forecast API
         var fiveDayForecastURL = "https://api.openweathermap.org/data/2.5/forecast?q=" + $(this).attr("data-city") + "&appid=";
-
+        
         $.ajax({
             method: "GET",
             url: fiveDayForecastURL + APIKEY
         }).then(function (response) {
             console.log("Length: ", response.list.length);
             // console.log("list 5 dt txt: ", response.list[5].dt_txt);
-            for (var i = 4; i < response.list.length ; i = i + 8) {
+            for (var i = 4; i < response.list.length; i = i + 8) {
                 console.log("i is: ", i);
                 console.log("list [i] dt txt: ", response.list[i].dt_txt);
-                console.log(response.list[i].dt_txt.substring(0,10));
+                console.log(response.list[i].dt_txt.substring(0, 10));
             }
-            card.append(collapse);
+
+            cardLink.append(collapse);
             collapse.append(cardBody);
             cardBody.append(row);
-            
+
 
         }); // end of AJAX call
     });
+
+    // *** FOURSQUARE API ***
+    var clientID = "KKWZ0AZRDFFQZVPRPXDQDFQGKKKVLSSPOIHYG0GXBIKFRRNN";
+    var clientSecret = "GQCLHQBZ5OFEWE4430YKBPETWT2535BAJWQW0D0RPYILV5GM";
+    var fourSquareURL = "https://api.foursquare.com/v2/venues/search?";
+    var newURL = "https://api.foursquare.com/v2/venues/explore?&near=Chicago&client_id=" 
+                + clientID + "&client_secret=" + clientSecret + "&v=20180508" + "&query=hotels";
+    $.ajax({
+        method: "GET",
+        url: newURL,
+        dataType: 'json',
+        // data: 'limit=5' +
+        //         'near=' + "Chicago,IL" +
+        //         '&?client_id=' + clientID +
+        //         '&client_secret=' + clientSecret +
+        //         '&v=20180508' +
+        //         '&query=' + "hotels"
+        
+    }).then(function (response) {
+        // console.log("fourSquareURL response: ", response);
+        // console.log("names of hotels", response.groups[0].items[7].venue.name);
+    });
+
 }); // end of document ready function
